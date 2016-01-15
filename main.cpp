05794c74f410553034581872bf67bb9873c3e266
steven99321023 <steven99321023@gmail.com>
2016-01-15 21:08:35 +0800

    Times Table first upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i(0);
+    int j(0);
+
+    for(i=1;i<10;i++)
+    {
+        for(j=1;j<10;j++)
+        {
+            cout<<i<<"*"<<j<<"="<<i*j<<" ";
+            if((i*j)<10)
+                cout<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
